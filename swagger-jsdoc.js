578ef33e97aaf1fe60e2117c6c5883f4003900fe
Amanda <Amanda@DESKTOP-0RF870V>
2020-03-25 14:43:34 +0000

    Remove the hard coded server

--- swagger-jsdoc.js
+++ swagger-jsdoc.js
@@ -10,12 +10,7 @@ const options = {
         name: "MIT",
         url: "https://choosealicense.com/licenses/mit/"
       },
-    },
-    servers: [
-      {
-        url: "http://localhost:3000/api/v1"
-      }
-    ]
+    },    
   },
   apis: ['./models/user.js', './routes/twitter.js']
 };
